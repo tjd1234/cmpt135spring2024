9225f3da00f6d5d9d41602ea9e0f3d171ac55c50
Toby Donaldson <tjd@sfu.ca>
2024-03-01 00:00:12 -0800

    updated

--- sample_code/week13/prebaked/cmpt_error.h
+++ sample_code/week13/prebaked/cmpt_error.h
@@ -5,21 +5,21 @@
 #ifndef CMPT_ERROR_H
 #define CMPT_ERROR_H
 
-#include <stdexcept>
 #include <string>
+#include <stdexcept>
 
 // C++ already has function called error, and so we put our error function
 // inside a namespace called cmpt. Thus, to use this error function, we will
 // write its full name, cmpt::error.
-namespace cmpt
+namespace cmpt {
+
+// runtime_error is a pre-defined C++ object meant to be "thrown" when an error
+// occurs while a program is running. When it is thrown, the program will end
+// and print the given error message.
+inline void error(const std::string& message)
 {
-    // runtime_error is a pre-defined C++ object meant to be "thrown" when an error
-    // occurs while a program is running. When it is thrown, the program will end
-    // and print the given error message.
-    inline void error(const std::string &message)
-    {
-        throw std::runtime_error(message);
-    }
+    throw std::runtime_error(message);
+}
 
 } // namespace cmpt
 
